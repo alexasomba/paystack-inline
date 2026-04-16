1be47b92a092fdf137c6cd214d14167fab4fb28a
Alexander Asomba <3323537+alexasomba@users.noreply.github.com>
2026-04-16 08:04:52 +0100

    fix: switch to pnpm and update workflow for master branch

--- git_push.sh
+++ git_push.sh
@@ -27,7 +27,7 @@ fi
 git init
 
 # Ensure the branch is named main
-git branch -M main
+git branch -M master
 
 # Adds the files in the local repository and stages them for commit.
 git add .
@@ -48,8 +48,8 @@ if [ "$git_remote" = "" ]; then # git remote not defined
 
 fi
 
-git pull origin main --allow-unrelated-histories
+git pull origin master --no-rebase --allow-unrelated-histories
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
-git push origin main 2>&1 | grep -v 'To https'
+git push origin master 2>&1 | grep -v 'To https'
